0492751f8e4a7c993a3809a61948346854e61792
Bruno <brunokoeke@gmail.com>
2025-06-11 10:22:14 -0300

    Corrigir problema de carregamento do vídeo e otimizar configuração

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,22 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    largePageDataBytes: 128 * 1000 * 1000, // 128MB
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*).webm',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          }
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
